dc07c0324d95c6d51c85cf1217e48df5c04dbc3e
HEROBRINE894 <mohapatrasudipta9@gmail.com>
2026-07-19 18:03:02 +0530

    Fix project filter

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.log("script.js loaded");
 function filterProjects(category) {
     const cards = document.querySelectorAll(".card");
+    const noProjects = document.getElementById("no-projects");
+    let visible = 0;
 
     cards.forEach(card => {
         if (category === "all" || card.classList.contains(category)) {
             card.style.display = "block";
+            visible++;
         } else {
             card.style.display = "none";
         }
     });
+
+    if (visible === 0) {
+        noProjects.style.display = "block";
+    } else {
+        noProjects.style.display = "none";
+    }
 }
